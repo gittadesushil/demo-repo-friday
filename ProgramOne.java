2acc425080360c782386dd3a2aee6035fae2e991
TestFirst <39798297+gittadesushil@users.noreply.github.com>
2021-08-13 16:15:41 +0200

    added new lines of code

--- ProgramOne.java
+++ ProgramOne.java
@@ -2,4 +2,8 @@ class ProgramOne{
     public static void main(String args[]){
         System.out.println("Hello how are you doing?");
     }
+
+    public static void MethodOne(String param1){
+        System.out.println("I am from MethodOne");
+    }
 }
