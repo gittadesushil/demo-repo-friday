fee5445d4feb33567ecba9b755f2d513afa20599
TestFirst <39798297+gittadesushil@users.noreply.github.com>
2021-08-13 16:19:47 +0200

    Added new java file

---
+++ ProgramTwo.java
@@ -0,0 +1,13 @@
+public class ProgramTwo {
+    public void MethodOne(){
+
+    }
+
+    public void MethodTwo(String pram1){
+
+    }
+
+    public String MethodThree(String param1){
+        return null;
+    }
+}
